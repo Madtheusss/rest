44793150fbafb7734c852e865333767102e28d8f
Matheus <madtheusss@hotmail.com>
2022-07-05 17:28:59 -0300

    criando a estrutura basica da api

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+
+const app = express();
+
+
+app.use((req,res,next) => {
+    res.status(200).send({
+        message:'Rodando'
+    })
+});
+
+
+module.exports = app;
